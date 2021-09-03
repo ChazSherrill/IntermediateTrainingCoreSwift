8d71775bfbbf09a4efa3e43cb2971c80091b3e99
Chaz Sherrill <chazsherrill@icloud.com>
2021-09-03 12:57:59 -0600

    hopefully this fixes everything

--- IntermediateTraining/CompaniesController.swift
+++ IntermediateTraining/CompaniesController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class CompaniesController: UITableViewController {
+class CompaniesController: UITableViewController, CreateCompanyControllerDelegate {
+    func didAddCompany(company: Company) {
+        companies.append(company) //1 - modify your array
+        let newIndexPath = IndexPath(row: companies.count - 1, section: 0)//2- insert a new index path into tableView
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+    }
+    
 
-    let companies = [
+    //let: constant
+    //var: variable that can be modified
+    var companies = [
         Company(name: "Apple", founded: Date()),
         Company(name: "Google", founded: Date()),
         Company(name: "Facebook", founded: Date())
     ]
     
+//    func addCompany(company: Company){
+//        //let tesla = Company(name: "Tesla", founded: Date())
+//
+//
+//        companies.append(company)
+//
+//        //2- insert a new index path into tableView
+//
+//        let newIndexPath = IndexPath(row: companies.count - 1, section: 0)
+//        tableView.insertRows(at: [newIndexPath], with: .automatic)
+//    }
+//
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //navigationItem.leftBarButtonItem = UIBarButtonItem(title: "TEST ADD", style: .plain, target: self, action: #selector(addCompany))
         
         view.backgroundColor = .white
         
@@ -41,6 +63,8 @@ class CompaniesController: UITableViewController {
        // createCompanyController.view.backgroundColor = .green
         
         let navController = CustomNavigationController(rootViewController: createCompanyController)
+        
+        createCompanyController.delegate = self //this bit of code links the companies controller to the createcompany controller by creating an instance of copanies controller inside createcompanycontroller
         
         present(navController, animated: true, completion: nil)
         
--- IntermediateTraining/CreateCompanyController.swift
+++ IntermediateTraining/CreateCompanyController.swift
@@ -8,16 +8,84 @@
 
 import UIKit
 
+//Custom Delegation
+
+protocol CreateCompanyControllerDelegate{
+    func didAddCompany(company: Company)
+}
+
 class CreateCompanyController: UIViewController{
+    
+    //not tightly -coupled by using delegate
+    var delegate: CreateCompanyControllerDelegate?
+    
+    //var companiesController: CompaniesController? //this links to the Companies controller (this has been replaced with the delegate line 13-15 and line 19)
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        //enable autolayout
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let nameTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter name"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
         
         navigationItem.title = "Creat Company"
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave))
+        
         view.backgroundColor = UIColor.darkBlue
+        }
+    
+    @objc private func handleSave(){
+        print("Trying to save company...")
+        
+        dismiss(animated: true){
+            guard let name = self.nameTextField.text else { return }
+            
+            let company = Company(name: name, founded: Date())
+            
+            self.delegate?.didAddCompany(company: company)
+            }
+        }
+    
+    private func setupUI() {
+        let lightBlueBackGroundView = UIView()
+        lightBlueBackGroundView.backgroundColor = UIColor.lightBlue
+        lightBlueBackGroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(lightBlueBackGroundView)
+        
+        lightBlueBackGroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        lightBlueBackGroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        lightBlueBackGroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        lightBlueBackGroundView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(nameLabel)
+        nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        nameLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(nameTextField)
+        nameTextField.leftAnchor.constraint(equalTo: nameLabel.rightAnchor).isActive = true
+        nameTextField.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        nameTextField.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
+        nameTextField.topAnchor.constraint(equalTo: nameLabel.topAnchor).isActive = true
+        
         
     }
     
